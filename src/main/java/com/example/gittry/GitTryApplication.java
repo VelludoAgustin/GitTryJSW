26d7762e37b5c4ffe8c8556eab921f8895a9623f
VelludoAgustin <agustin.v.2000@gmail.com>
2024-09-09 18:07:43 -0300

    first commit

---
+++ src/main/java/com/example/gittry/GitTryApplication.java
@@ -0,0 +1,14 @@
+package com.example.gittry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTryApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(GitTryApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
